80cef8716b52b6d56af1c529c89c036f77d4493f
Mubarak <desinamubarak123@gmail.com>
2024-06-10 00:58:01 +0100

    create the landingPage features component

--- frontend/src/pages/landingPage/landingPage.jsx
+++ frontend/src/pages/landingPage/landingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './components/Banner'
+import Features from './components/Features'
 
 export default function LandingPage() {
   return (
     <div>
         <Banner />
+        <Features />
     </div>
   )
 }
