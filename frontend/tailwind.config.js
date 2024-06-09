a09b238f7b3ca512aee16b6e7c18bbefdbdcf14d
Mubarak <desinamubarak123@gmail.com>
2024-06-09 23:29:21 +0100

    add default blue color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'blue': '#5958cf',
+          'secoundary': '#555',
+          'primary': '#FCFCFC'
+      }
+    },
   },
   plugins: [
     require('daisyui'),
