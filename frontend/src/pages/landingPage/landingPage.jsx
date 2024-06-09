7e17b62238ceaafb2d505c70fd30476c77d8790d
Mubarak <desinamubarak123@gmail.com>
2024-06-09 23:57:06 +0100

    create the banner component

--- frontend/src/pages/landingPage/landingPage.jsx
+++ frontend/src/pages/landingPage/landingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function landingPage() {
+export default function LandingPage() {
   return (
-    <div>landingPage</div>
+    <div>LandingPage</div>
   )
 }
